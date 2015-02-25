be11315f08cb87f33ada55e4fdfc2f8df1872daa
Phillip Oldham <phillip.oldham@gmail.com>
2015-02-25 20:13:32 +0000

    allowing the skipping of xcode-select when already installed

--- init.sh
+++ init.sh
@@ -2,8 +2,14 @@
 
 set -e
 
-echo "\033[0;34mInstalling XCode Command Line Tools...\033[0m"
-xcode-select --install
+echo "\033[0;34mYou'll need to enter your sudo password to continue...\033[0m"
+
+sudo echo "\033[0;34mInstalling XCode Command Line Tools...\033[0m"
+if [ -f /usr/bin/gcc ]; then
+    echo "\033[0;34m... already installed.\033[0m"
+else
+    xcode-select --install
+fi
 
 echo "\033[0;34mInstalling pip...\033[0m"
 curl -L https://bootstrap.pypa.io/get-pip.py | sudo python
